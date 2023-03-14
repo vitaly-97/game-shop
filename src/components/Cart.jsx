793255c9575ce6928ace9d71fcbfd38d5f3e7a94
Vitaly Kurenkov <vitamin1551@mail.ru>
2023-03-14 18:52:47 +0300

    commit-2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,12 @@
-function Cart(props) {
-  const { quantity = 0, handleBasketShow = Function.prototype } = props;
+import { useContext } from "react";
+import { ShopContext } from "../context";
+
+function Cart() {
+  const { order, handleBasketShow = Function.prototype } =
+    useContext(ShopContext);
+
+  const quantity = order.length;
+
   return (
     <div className="cart blue darken-4 white-text" onClick={handleBasketShow}>
       <i className="material-icons">shopping_cart</i>
